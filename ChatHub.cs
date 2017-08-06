85ca81144dd868033f44d5f7231d9c96c94a7f62
Ibro <ibro@ibro.me>
2017-08-06 11:51:36 +0200

    adding simple Chat Hub

---
+++ ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRSimpleChat
+{
+    public class Chat : Hub
+    {      
+        public async Task Send(string message)
+        {
+            await Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
