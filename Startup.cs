dd1bb1877af65cca52271040b7fe98874fbacdbc
Ibro <ibro@ibro.me>
2017-10-24 20:51:25 +0200

    Update to latest SignalR - 1.0.0-alpha2-final

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace SignalRSimpleChat
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {         
             app.UseStaticFiles();
 
